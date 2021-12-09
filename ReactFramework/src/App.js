6511853092d44bad393e4e58bf0d6514a57d84f3
odzen <jsebastian.va@gmail.com>
2021-12-08 21:46:29 -0500

    Bases de React, Hola Mundo y componentes sintaxis

--- ReactFramework/src/App.js
+++ ReactFramework/src/App.js
@@ -1,14 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+//Importar componentes
+import MiComponente from './componentes/MiComponente';
+import Peliculas from './componentes/Peliculas';
+
+function HolaMundo(nombre, edad){
+  // Esta variable nombre no es una string sino que un objeto de JSX
+  var presentacion=  (
+    <div>
+      <h2>Hola, soy {nombre} </h2>
+      <h3>Tengo {edad} años </h3>
+    </div>);
+  return presentacion;
+}
 
 function App() {
+
+  var nombre= "Sebastian";
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Mundo
         </p>
+
+
+        {HolaMundo(nombre, 21)}
+
+        {alert("Hola Mundo")}
+
+        <section className="componentes">
+          <MiComponente />
+          <Peliculas/>
+        </section>
+
         <a
           className="App-link"
           href="https://reactjs.org"
